a7b685938b3257e27dfd32a58735263059b15a82
Aarish Shah Mohsin <aarishshah1@gmail.com>
2025-01-10 19:44:26 +0530

    fix

--- a.py
+++ a.py
@@ -6,12 +6,13 @@ df = pd.read_csv("terms_355.csv")
 
 for idx, row in df.iterrows():
     if row["total"] == 200:
-        row["total"] /= 2
-        row["male_count"] /= 2
-        row["female_count"] /= 2
-    row["male_percentage"] = (
+        df.loc[idx, "total"] /= 2
+        df.loc[idx, "male_count"] /= 2
+        df.loc[idx, "female_count"] /= 2
+    df.loc[idx, "male_percentage"] = (
         row["male_count"] / (row["female_count"] + row["male_count"]) * 100
-     
+    )
+
 
 df["male_percentage"] = df["male_percentage"].apply(lambda x: round(x, 3))
 
